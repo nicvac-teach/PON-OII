0a7973ff55a2eb1432083fe1fb7703e4edc39cdc
nicvac-teach <nicola.vacca@marconibari.edu.it>
2025-02-24 12:46:29 +0000

    collezionismo

--- collezionismo.cpp
+++ collezionismo.cpp
@@ -13,6 +13,18 @@ using namespace std;
 
 #define ull unsigned long long
 
+//Struttura di appoggio, per conservare la discrepanza e il relativo indice.
+class E {
+public:
+    E(int d, int i): diff(d), idx(i) {}
+    int diff;
+    int idx;
+
+    bool operator<(const E & other) const {
+        return diff < other.diff;
+    }
+};
+
 void solve(int t) {
     int N, K;
     cin >> N >> K;
@@ -24,137 +36,82 @@ void solve(int t) {
 
     // aggiungi codice...
 
-    /* Intuizione chiave:
-    Se sapessimo che la risposta è X, potremmo verificare se è possibile ottenere una soluzione 
-    dove la somma delle discrepanze è ≤ X
-    Questo suggerisce che possiamo fare una ricerca binaria sulla risposta
-    Cioè: - se non trovo una combinazione, provo per X più grande
-          - se trovo la combinazione, provo per X più piccolo
-    Bisogna ricercare un valore di X in [0, max(C)-min(C)]
-    Cominciamo con un valore medio di X.
+    /*
+    L'idea è quella di raggruppare il più possibile tutti insieme elementi simili tra loro,
+    Gli elementi a cavallo di un picco non devono essere sulla stessa mensola.
 
-    > Funzione di verifica per un dato X
-    - Per un dato X (somma totale target): Dobbiamo solo garantire che la SOMMA delle discrepanze sia ≤ X
-    - Provare a formare gruppi in modo greedy
-    - Tenere traccia della somma delle discrepanze
-    - Fermarsi se:
-        - La somma supera X (fallimento)
-        - Abbiamo usato più di K gruppi (fallimento)
-        - Abbiamo assegnato tutti gli elementi usando ≤ K gruppi (successo)
+    6 3
+    4 42 23 0 21 2
 
-    - Se riusciamo a formare ≤ K gruppi, SOMMA≤ X ==> X è possibile, altrimenti X non è possibile.
+    6 robot, 3 scaffali
+    ordino i robot
+    0 2 4 21 23 42
 
-    Esempio
-    Per l'array ordinato [0, 2, 4, 21, 23, 42] e K=3:
+    calcolo le differenze, conservando gli indici
+    2(0,2) 2(2,4) 17(4,21) 2(21,23) 19(23,42)
+      0 1    1 2     2 3     3  4      4  5 
 
-    - Provo a costruire gruppi in modo greedy:
-    * Ogni gruppo può avere qualsiasi discrepanza
-    * Mi fermo quando la somma delle discrepanze supera X
-    * Devo solo garantire di usare al massimo K gruppi
+    Ordino le differeze, conservando gli indici degli elementi a cui si riferiscono
+    2 2 2 17 19
+    2(0,2) 2(2,4) 2(21,23) 17(4,21) 19(23,42)
+      0 1    1 2     3  4      2 3<-    4  5<- 
 
-    Per X=6:
-    Gruppo 1: [0, 2, 4]     -> discr = 4
-    Gruppo 2: [21, 23]      -> discr = 2
-    Gruppo 3: [42]          -> discr = 0
-                            Tot = 6 ≤ X
-    
-    Prima iterazione
-    min_gruppo = 0
-    Provo ad aggiungere:
-    - 2: discr = 2, ok (somma = 2)
-    - 4: discr = 4, ok (somma = 4)
-    - 21: discr = 21, NO!
-    Prendo [0,2,4]
-        
-    Seconda iterazione
-    min_gruppo = 21
-    Provo ad aggiungere:
-    - 23: discr = 2, ok (somma = 6)
-    - 42: discr = 21, NO!
-    Prendo [21,23]
-
-    Terza iterazione
-    min_gruppo = 42
-    gruppo = [42]
-    discr = 0 (somma resta 6)
+    Ho 3 mensole, quindi devo partizionare C in 3 parti, quindi metto 2 separatori
+    per definire le 3 parti. Dove le colloco? Ovviamente su C[5] e C[3], gli indici
+    relativi alle differenze più alte. In questo modo su una stessa mensola
+    non ci sono elementi che mi danno discrepanza di 19 e 17, le discrepanze più alte. 
     */
-
+    
     //Ordino i valori per calcolare le discrepanze fra valori contigui
     sort(C.begin(), C.end());
 
-    pair<ull, ull> rangeX( make_pair(0, C.back() - C.front()) );
-    pair<ull, ull> rangeX_prev = rangeX;
-    bool loop = false;
-    while ( rangeX.first <= rangeX.second && not(loop) ) {
-        //size_t X = (size_t) round((rangeX.first + rangeX.second) / 2.0);
-        ull X = (rangeX.first + rangeX.second) / 2;
-
-        //La somma delle K discrepanze deve essere X ==> provo a mettere più elementi
-        // possibili nel gruppo finchè resto in somma X
-
-        //Ogni elemento di idx corrisponde a una mensola
-        // se idx è [0,4,9]  ==> ho tre mensole. La prima mensola contiene da C[0] a c[3],
-        // la seconda mensola contiene da C[4] a C[8], la terza da C[9] in poi
-        vector<size_t> idx(1, 0);
-        size_t i = 1;
-        ull somma = 0;
-        //Se idx supera K ==> X corrente non è possibile
-        //Se somma supera X ==> X corrente non è possibile
-        while ( i < C.size() && idx.size() <= K && somma <= X ) {
-            //discrepanza sulla mensola corrente
-            ull d = C[i] - C[ idx.back() ];
-            if (somma + d > X  ) {
-                //La discrepanza della mensola corrente fa crescere la discrepanza totale
-                // tale da superare X ==> dispongo l'elemento corrente sulla nuova mensola.
-                // Ho completato una mensola ==> aggiungo la sua discrepanza al totale.
-                somma += C[i-1] - C[idx.back()];
-                //Nella nuova mensola dispongo l'elemento i
-                idx.push_back(i);
-            }
-            ++i;
-        }
-        //Calcolo e sommo la Discrepanza dell'ultima mensola
-        somma += C.back() - C[idx.back()];
-        //Se ho analizzato tutti gli elementi e ho impiegato K mensole 
-        // (cioè in idx ho K elementi) => è possibile disporre su K mensole 
-        // con somma delle discrepanze pari a X, cioè X è possibile.
-        if ( i==C.size() && idx.size() <= K && somma <= X ) {
-            //X è possibile, provo per X più piccole.
-            //Se somma è più piccolo di X, considero somma.
-            rangeX.second = X;
-            //rangeX.second = min(X, somma);
-        } else {
-            //X non è possibile, provo per X più grandi.
-            //rangeX.first = X+1;
-            rangeX.first = X;
-        }
-
-        loop = (rangeX == rangeX_prev);
-        rangeX_prev = rangeX;
+    vector<E> discrepanze;
+    for (int i=0; i<=C.size()-2; ++i) {
+        discrepanze.push_back(E(C[i+1]-C[i], i+1));
     }
-    
-    //Il limite superiore contiene le X possibili
-    int risposta = rangeX.second;
+
+    //Ordino le discrepanze
+    sort(discrepanze.begin(), discrepanze.end());
+
+    //K mensole => Piazzo k-1 separatori
+    vector<size_t> separatori;
+    int j=discrepanze.size()-1;
+    for (int i=1; i<=K-1; ++i) {
+        separatori.push_back( discrepanze[j].idx );
+        --j;
+    }
+    //Ordino i separatori, da piazzare su C
+    sort(separatori.begin(), separatori.end());
+
+    //Calcolo x, la somma delle discrepanze;
+    size_t x = 0;
+    size_t idxA = 0;
+    for (int i=0; i<separatori.size(); ++i) {
+        size_t idxB = separatori[i]-1;
+        x += C[idxB] - C[idxA];
+        idxA = separatori[i];
+    }
+    x += C.back() - C[idxA];
+
+    int risposta = x;
 
     cout << "Case #" << t << ": " << risposta << "\n";
     
 }
 
 /*
-ATTENZIONE! SCORE 4/15!
-NON FUNZIONA PER TUTTI I TEST CASE
-Soluzione da wiki oii:
+Soluzione basata su wiki oii:
 https://wiki.olinfo.it/2021/territoriali/collezionismo
 https://forum.olinfo.it/t/chiarimenti-per-il-codice-di-collezionismo/8201/4
+La mia versione è più intuitiva e meno matematica, ma si basa sullo stesso criterio.
 */
 
 int main() {
     // se preferisci leggere e scrivere da file
     // ti basta decommentare le seguenti due righe:
 
-    //freopen("collezionismo_input_1.txt", "r", stdin);
-    //freopen("collezionismo_input_8.txt", "r", stdin);
-    freopen("collezionismo_input01.txt", "r", stdin);
+    freopen("collezionismo_input_1.txt", "r", stdin);
+    //freopen("collezionismo_input01.txt", "r", stdin);
     freopen("output.txt", "w", stdout);
 
     int T;
---
+++ collezionismo.subopt.cpp
@@ -0,0 +1,168 @@
+#include <cstdio>
+#include <cstring>
+#include <algorithm>
+#include <fstream>
+#include <iostream>
+#include <numeric>
+#include <set>
+#include <utility>
+#include <vector>
+#include <cmath>
+
+using namespace std;
+
+#define ull unsigned long long
+
+void solve(int t) {
+    int N, K;
+    cin >> N >> K;
+
+    vector<int> C(N);
+    for (int i = 0; i < N; i++) {
+        cin >> C[i];
+    }
+
+    // aggiungi codice...
+
+    /* Intuizione chiave:
+    Se sapessimo che la risposta è X, potremmo verificare se è possibile ottenere una soluzione 
+    dove la somma delle discrepanze è ≤ X
+    Questo suggerisce che possiamo fare una ricerca binaria sulla risposta
+    Cioè: - se non trovo una combinazione, provo per X più grande
+          - se trovo la combinazione, provo per X più piccolo
+    Bisogna ricercare un valore di X in [0, max(C)-min(C)]
+    Cominciamo con un valore medio di X.
+
+    > Funzione di verifica per un dato X
+    - Per un dato X (somma totale target): Dobbiamo solo garantire che la SOMMA delle discrepanze sia ≤ X
+    - Provare a formare gruppi in modo greedy
+    - Tenere traccia della somma delle discrepanze
+    - Fermarsi se:
+        - La somma supera X (fallimento)
+        - Abbiamo usato più di K gruppi (fallimento)
+        - Abbiamo assegnato tutti gli elementi usando ≤ K gruppi (successo)
+
+    - Se riusciamo a formare ≤ K gruppi, SOMMA≤ X ==> X è possibile, altrimenti X non è possibile.
+
+    Esempio
+    Per l'array ordinato [0, 2, 4, 21, 23, 42] e K=3:
+
+    - Provo a costruire gruppi in modo greedy:
+    * Ogni gruppo può avere qualsiasi discrepanza
+    * Mi fermo quando la somma delle discrepanze supera X
+    * Devo solo garantire di usare al massimo K gruppi
+
+    Per X=6:
+    Gruppo 1: [0, 2, 4]     -> discr = 4
+    Gruppo 2: [21, 23]      -> discr = 2
+    Gruppo 3: [42]          -> discr = 0
+                            Tot = 6 ≤ X
+    
+    Prima iterazione
+    min_gruppo = 0
+    Provo ad aggiungere:
+    - 2: discr = 2, ok (somma = 2)
+    - 4: discr = 4, ok (somma = 4)
+    - 21: discr = 21, NO!
+    Prendo [0,2,4]
+        
+    Seconda iterazione
+    min_gruppo = 21
+    Provo ad aggiungere:
+    - 23: discr = 2, ok (somma = 6)
+    - 42: discr = 21, NO!
+    Prendo [21,23]
+
+    Terza iterazione
+    min_gruppo = 42
+    gruppo = [42]
+    discr = 0 (somma resta 6)
+    */
+
+    //Ordino i valori per calcolare le discrepanze fra valori contigui
+    sort(C.begin(), C.end());
+
+    pair<ull, ull> rangeX( make_pair(0, C.back() - C.front()) );
+    pair<ull, ull> rangeX_prev = rangeX;
+    bool loop = false;
+    while ( rangeX.first <= rangeX.second && not(loop) ) {
+        //size_t X = (size_t) round((rangeX.first + rangeX.second) / 2.0);
+        ull X = (rangeX.first + rangeX.second) / 2;
+
+        //La somma delle K discrepanze deve essere X ==> provo a mettere più elementi
+        // possibili nel gruppo finchè resto in somma X
+
+        //Ogni elemento di idx corrisponde a una mensola
+        // se idx è [0,4,9]  ==> ho tre mensole. La prima mensola contiene da C[0] a c[3],
+        // la seconda mensola contiene da C[4] a C[8], la terza da C[9] in poi
+        vector<size_t> idx(1, 0);
+        size_t i = 1;
+        ull somma = 0;
+        //Se idx supera K ==> X corrente non è possibile
+        //Se somma supera X ==> X corrente non è possibile
+        while ( i < C.size() && idx.size() <= K && somma <= X ) {
+            //discrepanza sulla mensola corrente
+            ull d = C[i] - C[ idx.back() ];
+            if (somma + d > X  ) {
+                //La discrepanza della mensola corrente fa crescere la discrepanza totale
+                // tale da superare X ==> dispongo l'elemento corrente sulla nuova mensola.
+                // Ho completato una mensola ==> aggiungo la sua discrepanza al totale.
+                somma += C[i-1] - C[idx.back()];
+                //Nella nuova mensola dispongo l'elemento i
+                idx.push_back(i);
+            }
+            ++i;
+        }
+        //Calcolo e sommo la Discrepanza dell'ultima mensola
+        somma += C.back() - C[idx.back()];
+        //Se ho analizzato tutti gli elementi e ho impiegato K mensole 
+        // (cioè in idx ho K elementi) => è possibile disporre su K mensole 
+        // con somma delle discrepanze pari a X, cioè X è possibile.
+        if ( i==C.size() && idx.size() <= K && somma <= X ) {
+            //X è possibile, provo per X più piccole.
+            //Se somma è più piccolo di X, considero somma.
+            rangeX.second = X;
+            //rangeX.second = min(X, somma);
+        } else {
+            //X non è possibile, provo per X più grandi.
+            //rangeX.first = X+1;
+            rangeX.first = X;
+        }
+
+        loop = (rangeX == rangeX_prev);
+        rangeX_prev = rangeX;
+    }
+    
+    //Il limite superiore contiene le X possibili
+    int risposta = rangeX.second;
+
+    cout << "Case #" << t << ": " << risposta << "\n";
+    
+}
+
+/*
+ATTENZIONE! SCORE 4/15!
+NON FUNZIONA PER TUTTI I TEST CASE.
+Lo tengo perchè è un esempio di riceca binaria con strategia greedy, 
+che trova una soluzione subottima, ma non sempre l'ottima in assoluto.
+Per la soluzione a questo problema vedi collezionismo.cpp
+*/
+
+int main() {
+    // se preferisci leggere e scrivere da file
+    // ti basta decommentare le seguenti due righe:
+
+    //freopen("collezionismo_input_1.txt", "r", stdin);
+    //freopen("collezionismo_input_8.txt", "r", stdin);
+    freopen("collezionismo_input01.txt", "r", stdin);
+    freopen("output.txt", "w", stdout);
+
+    int T;
+    cin >> T;
+
+    for (int t = 1; t <= T; t++) {
+        solve(t);
+    }
+
+    return 0;
+}
